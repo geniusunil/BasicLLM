969debac5fee38e8153c49cb3fb2c67d668e2308
geniusunil <geniusunil@zohomail.in>
2025-11-11 17:31:41 +0530

    before lecture 8 BPE

--- finalLLMcode/main.py
+++ finalLLMcode/main.py
@@ -1,16 +1,28 @@
+# import importlib
 import re
-from simple_tokenizer import SimpleTokenizerV1
+import tiktoken
+from simple_tokenizer import SimpleTokenizerV2
+
+# import importlib
+# print(importlib.__file__)
+
+# print("tiktoken version:", importlib.metadata.version("tiktoken"))
 
 with open("../the-verdict.txt", "r", encoding="utf-8") as f:
     raw_text = f.read()
-    
-result = re.split(r'(\s)', raw_text)
+# text = raw_text.join(" <|endoftext|> ")
 
-print(result)
+preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', raw_text)
+preprocessed = [item.strip() for item in preprocessed if item.strip()]
+all_words = sorted(set(preprocessed))
+# result = re.split(r'(\s)', raw_text)
+all_words.extend(["<|endoftext|>", "<|unk|>"])
+# print(result)
+vocab = {token:integer for integer,token in enumerate(all_words)}
 
-# tokenizer = SimpleTokenizerV1(vocab)
+tokenizer = SimpleTokenizerV2(vocab)
 
 # text = """"It's the last he painted, you know," 
 #            Mrs. Gisburn said with pardonable pride."""
-# ids = tokenizer.encode(text)
-# print(ids)
+ids = tokenizer.encode(raw_text + " <|endoftext|> ")
+print(ids)
