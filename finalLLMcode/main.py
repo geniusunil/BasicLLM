b7802fd3ad08e553d846d817287ec89988b5b759
geniusunil <geniusunil@zohomail.in>
2025-11-19 17:18:09 +0530

    lecture 8 tiktoken bpe

--- finalLLMcode/main.py
+++ finalLLMcode/main.py
@@ -12,17 +12,27 @@ with open("../the-verdict.txt", "r", encoding="utf-8") as f:
     raw_text = f.read()
 # text = raw_text.join(" <|endoftext|> ")
 
-preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', raw_text)
-preprocessed = [item.strip() for item in preprocessed if item.strip()]
-all_words = sorted(set(preprocessed))
-# result = re.split(r'(\s)', raw_text)
-all_words.extend(["<|endoftext|>", "<|unk|>"])
-# print(result)
-vocab = {token:integer for integer,token in enumerate(all_words)}
+# preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', raw_text)
+# preprocessed = [item.strip() for item in preprocessed if item.strip()]
+# all_words = sorted(set(preprocessed))
+# # result = re.split(r'(\s)', raw_text)
+# all_words.extend(["<|endoftext|>", "<|unk|>"])
+# # print(result)
+# vocab = {token:integer for integer,token in enumerate(all_words)}
 
-tokenizer = SimpleTokenizerV2(vocab)
+# tokenizer = SimpleTokenizerV2(vocab)
 
-# text = """"It's the last he painted, you know," 
-#            Mrs. Gisburn said with pardonable pride."""
-ids = tokenizer.encode(raw_text + " <|endoftext|> ")
-print(ids)
+# # text = """"It's the last he painted, you know," 
+# #            Mrs. Gisburn said with pardonable pride."""
+# ids = tokenizer.encode(raw_text + " <|endoftext|> ")
+# print(ids)
+
+tokenizer = tiktoken.get_encoding("gpt2")
+
+integers = tokenizer.encode(raw_text, allowed_special={"<|endoftext|>"})
+
+# print(integers)
+
+strings = tokenizer.decode(integers)
+
+print(strings)
