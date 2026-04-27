e1baf3e4b2bc92bf38263f40cc17e9c991762513
geniusunil <geniusunil@zohomail.in>
2026-04-27 15:54:34 +0530

    lecture 12 30 minutes

--- finalLLMcode/main.py
+++ finalLLMcode/main.py
@@ -13,13 +13,13 @@ with open("../the-verdict.txt", "r", encoding="utf-8") as f:
     raw_text = f.read()
 # text = raw_text.join(" <|endoftext|> ")
 
-# preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', raw_text)
-# preprocessed = [item.strip() for item in preprocessed if item.strip()]
-# all_words = sorted(set(preprocessed))
+preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', raw_text)
+preprocessed = [item.strip() for item in preprocessed if item.strip()]
+all_words = sorted(set(preprocessed))
 # # result = re.split(r'(\s)', raw_text)
-# all_words.extend(["<|endoftext|>", "<|unk|>"])
+all_words.extend(["<|endoftext|>", "<|unk|>"])
 # # print(result)
-# vocab = {token:integer for integer,token in enumerate(all_words)}
+vocab = {token:integer for integer,token in enumerate(all_words)}
 
 # tokenizer = SimpleTokenizerV2(vocab)
 
@@ -32,16 +32,37 @@ with open("../the-verdict.txt", "r", encoding="utf-8") as f:
 
 # integers = tokenizer.encode(raw_text, allowed_special={"<|endoftext|>"})
 # gptdataset = GPTDatasetV1(raw_text)
-print("PyTorch version:", torch.__version__)
+# print("PyTorch version:", torch.__version__)
 dataloader = GPTDatasetV1.create_dataloader_v1(
     raw_text, batch_size=1, max_length=4, stride=1, shuffle=False
 )
 
-data_iter = iter(dataloader)
-first_batch = next(data_iter)
-print(first_batch)
+# data_iter = iter(dataloader)
+# first_batch = next(data_iter)
+# print(first_batch)
 # print(integers)
 
 # strings = tokenizer.decode(integers)
 
 # print(strings)
+# input_ids = torch.tensor([2, 3, 5, 1])
+# vocab_size = 6
+# output_dim = 3
+
+# torch.manual_seed(123)
+# embedding_layer = torch.nn.Embedding(vocab_size, output_dim)
+
+# print(embedding_layer.weight)
+tokenizer = SimpleTokenizerV2(vocab)
+text1 = "Hello, do you like tea?"
+text2 = "In the sunlit terraces of the palace."
+
+text = " <|endoftext|> ".join((text1, text2))
+
+print(text)
+
+code = tokenizer.encode(text)
+
+print(code)
+
+print(tokenizer.decode(code))
